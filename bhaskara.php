bd8f1dc80298c8439cb48654c34a9b06991ed0cd
brandonhenrique <brandonhenrique36@gmail.com>
2021-08-13 19:31:01 -0300

    Alterações Finais

--- bhaskara.php
+++ bhaskara.php
@@ -1,53 +1,45 @@
 <?php
     
-  include 'funcoes.php';
+    include('funcoes.php');
     
-  $a = $_POST['txta'];
-  $b = $_POST['txtb'];
-  $c = $_POST['txtc'];
+    $a = $_POST['txta'];
+    $b = $_POST['txtb'];
+    $c = $_POST['txtc'];
 
-  $a_fim = $_POST['txta_fim'];
-  $b_fim = $_POST['txtb_fim'];
-  $c_fim = $_POST['txtc_fim'];
-
-  
-  $a_valors = array();
-  $b_valors = array();
-  $c_valors = array();
-
-  for ($i = $a; $i <= $a_fim; $i++) { 
-    $a_valors[] = $i;
-  }
-
-  for ($i = $b; $i <= $b_fim; $i++) { 
-    $b_valors[] = $i;
-  }
-
-  for ($i = $c; $i <= $c_fim; $i++) { 
-    $c_valors[] = $i;
-  }
+    $a_fim = $_POST['txta_fim'];
+    $b_fim = $_POST['txtb_fim'];
+    $c_fim = $_POST['txtc_fim'];
+    
 
 
-  //var_dump($a_valors);
+    $valor_a = array();
+    $valor_b = array();
+    $valor_c = array();
 
-  //var_dump($b_valors);
+    for ($i = $a; $i <= $a_fim ; $i++) {
+        $valor_a[] = $i;
+    }
+    for ($i = $b; $i <= $b_fim; $i++) {
+        $valor_b[] = $i;
+    }
+    
+    for ($i = $c; $i <= $c_fim; $i++) {
+        $valor_c[] = $i;
+    }
 
-  //var_dump($c_valors);
-  
-  $results = array();
+   $resultados = array();
 
-  foreach ($a_valors as $key_a => $value_a) {
-    foreach ($b_valors as $key_b => $value_b) {
-      foreach ($c_valors as $key_c => $value_c) {
-        $results_temp = calc_bhaskara($value_a, $value_b, $value_c);
-        if ($results_temp != NULL && $results_temp['x1'] != NULL && $results_temp['x2'] != NULL) {
-          $results[] = $results_temp;
-        }
+
+   foreach ($valor_a as $chave_a => $value_a) {
+    foreach ($valor_b as $chave_b => $value_b) {
+      foreach ($valor_c as $chave_c => $value_c) {
+        $resultado_temp = calc_bhaskara($valor_a, $valor_b, $valor_c);
+        if($resultado_temp != NULL && $resultado_temp['x1'] != NULL && $resultado_temp['x2'] != NULL){
+        $resultados[] = $resultado_temp;
+      }
       }
     }
   }
-  
-  var_dump($results);
 
-
+    
 ?>
--- funcoes.php
+++ funcoes.php
@@ -1,12 +1,70 @@
 <?php
 
-    function delta($vlr_a, $vlr_b, $vlr_c)
+    function calc_delta($a_vlr, $b_vlr, $c_vlr)
     {
-        $ret_delta = ($vlr_b * $vlr_b) - (4 * $vlr_a * $vlr_c);
+        $ret_delta = ($b_vlr * $b_vlr) - (4 * $a_vlr * $c_vlr);
 
         return $ret_delta;
     }
 
+    function calc_x1($a_vlr, $b_vlr, $delta_vlr)
+    {
+        $x1_ret = (-$b_vlr + sqrt($delta_vlr)) / (2 * $a_vlr);
 
+        return $x1_ret;
+    }
+
+    function calc_x2($a_vlr, $b_vlr, $delta_vlr)
+    {
+        $x2_ret = (-$b_vlr - sqrt($delta_vlr)) / (2 * $a_vlr);
+
+        return $x2_ret;
+    }
+
+    function calc_bhaskara($a_vlr, $b_vlr, $c_vlr)
+    {
+        if($a_vlr == 0) {
+
+        return NULL;
+    }
+
+    $ret = array();
+    $ret['delta'] = calc_delta($a_vlr, $b_vlr, $c_vlr);
+
+    if($ret['delta'] < 0)   {
+        $ret['x1'] = NULL;
+        $ret['x2'] = NULL;
+
+        return $ret;
+    }else if($ret['delta'] == 0) {
+        $ret['x1'] = calc_x1($a_vlr, $b_vlr, $ret['delta']);
+        $ret['x2'] = NULL;
+
+        return $ret;
+    }else {
+        $ret['x1'] = calc_x1($a_vlr, $b_vlr, $ret['delta']);
+        $ret['x2'] = calc_x2($a_vlr, $b_vlr, $ret['delta']);
+    }
+
+    $delta = $ret['delta'];
+    $x1 = $ret['x1'];
+    $x2 = $ret['x2'];
+    connect_and_write_db($a_vlr, $b_vlr, $c_vlr, $delta, $x1, $x2);
+
+    return $ret;
+    }
+
+    function get_insert_sql($a_vlr, $b_vlr, $c_vlr, $delta, $x1, $x2)
+    {
+        $query = "insert into formula_bhaskara (a, b, c, delta, x1, x2) values ('$a_vlr','$b_vlr','$c_vlr','$delta','$x1', '$x2')";
+        return($query);
+    }
+
+    function connect_and_write_db($a_vlr, $b_vlr, $c_vlr, $delta, $x1, $x2){
+        $conn = mysqli_connect('localhost', 'root', '', 'formula_bhaskara');
+        $query = get_insert_sql($a_vlr, $b_vlr, $c_vlr, $delta, $x1, $x2);
+        $result = mysqli_query($conn, $query);
+        return $result;
+    }
 
 ?>
